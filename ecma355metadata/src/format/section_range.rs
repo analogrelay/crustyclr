b9ea73652287e01c2d25cf2444d425215ff3e0c1
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-11-02 08:58:37 -0700

    clean up output a bit

--- ecma355metadata/src/format/section_range.rs
+++ ecma355metadata/src/format/section_range.rs
@@ -6,7 +6,7 @@ use error::Error;
 
 pub struct SectionRange {
     rva: u32,
-    size: u32
+    size: u32,
 }
 
 impl SectionRange {
@@ -18,10 +18,14 @@ impl SectionRange {
             size: buf.read_u32::<LittleEndian>()?,
         })
     }
+
+    pub fn end(&self) -> u32 {
+        self.rva + self.size
+    }
 }
 
 impl ::std::fmt::Display for SectionRange {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {
-        write!(f, "0x{:08X} (Size: 0x{:08X})", self.rva, self.size)
+        write!(f, "0x{:08X} - 0x{:08X}", self.rva, self.end())
     }
 }
